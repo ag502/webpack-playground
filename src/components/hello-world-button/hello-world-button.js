1aa49c97b3999e0813da851c5ff01c0414b7d89d
ag502 <ag502@naver.com>
2022-12-14 00:49:53 +0900

    ADD: loader

---
+++ src/components/hello-world-button/hello-world-button.js
@@ -0,0 +1,22 @@
+import "./hello-world-button.scss";
+
+class HelloWorldButton {
+  buttonCssClass = "hello-world-text";
+
+  render() {
+    const button = document.createElement("button");
+    button.innerHTML = "Hello World";
+    button.classList.add("hello-world-button");
+    const body = document.querySelector("body");
+
+    button.onclick = () => {
+      const p = document.createElement("p");
+      p.innerHTML = "Hello world";
+      p.classList.add(this.buttonCssClass);
+      body.appendChild(p);
+    };
+    body.appendChild(button);
+  }
+}
+
+export default HelloWorldButton;
--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,25 @@ module.exports = {
         test: /\.txt$/,
         type: "asset/source",
       },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: ["@babel/plugin-proposal-class-properties"],
+          },
+        },
+      },
     ],
   },
 };
